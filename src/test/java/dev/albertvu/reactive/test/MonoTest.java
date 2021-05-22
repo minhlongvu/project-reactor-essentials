c865cfbaa37db080235e6c05f5c429ca2db37c3e
Albert Vu <44858018+minhlongvu@users.noreply.github.com>
2021-05-21 21:40:30 -0500

    Project setup with lots of dependency additions. New package in test with class that contains a publisher that is being subscribed to. And a test that subscribes to the same publisher.

---
+++ src/test/java/dev/albertvu/reactive/test/MonoTest.java
@@ -0,0 +1,23 @@
+package dev.albertvu.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class MonoTest {
+	@Test
+	public void monoSubscriber() {
+		String name = "Albert Vu";
+		Mono<String> mono = Mono.just(name)
+				.log();
+
+		mono.subscribe();
+		log.info("----------------------------------------------------");
+
+		StepVerifier.create(mono)
+				.expectNext(name)
+				.verifyComplete();
+	}
+}
